0aaf59b6b000fa44e4ad5a3c4e3df443504405d7
Pedro Rico <pedro.rico@kellogg.northwestern.edu>
2025-03-12 17:02:22 +0000

    Sucess implementation of sign-up and login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,31 @@ class SessionsController < ApplicationController
   end
 
   def create
+    
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        # Login the user
+        session["user_id"] = @user["id"]
+        flash["notice"] = "WELCOME! You've logged in."
+        redirect_to "/places"
+      else
+        flash["notice"] = "NOPE! Unsuccessful login."
+        redirect_to "/sessions/new"
+      end
+
+    else
+      flash["notice"] = "NOPE! Unsuccessful login."
+      redirect_to "/sessions/new"
+    end
+
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Goodbye fella!"
+    redirect_to "/sessions/new"
+
   end
 end
   
