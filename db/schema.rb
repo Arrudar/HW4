f0351b5fcf3926e3110ea2a2c402a8068fc03d16
Pedro Rico <pedro.rico@kellogg.northwestern.edu>
2025-03-12 22:38:37 +0000

    Fixed error with entries

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema[7.1].define(version: 2022_03_09_020701) do
     t.text "description"
     t.date "occurred_on"
     t.integer "place_id"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
