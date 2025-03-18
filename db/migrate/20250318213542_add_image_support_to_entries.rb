6ee4a8ccfc2697b708b2d11c65c86496b40330f0
Pedro Rico <pedro.rico@kellogg.northwestern.edu>
2025-03-18 21:41:13 +0000

    Fix image attechment error

---
+++ db/migrate/20250318213542_add_image_support_to_entries.rb
@@ -0,0 +1,5 @@
+class AddImageSupportToEntries < ActiveRecord::Migration[7.1]
+  def change
+      add_column :entries, :image, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_12_232416) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_18_213542) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_12_232416) do
     t.integer "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "places", force: :cascade do |t|
